ae99c6ca418f1abf58bde47f109fee81a9965489
ntaheij <noah@taheij.nl>
2021-10-25 13:01:43 +0200

    Fixed routing

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -95,7 +95,7 @@ function Sidebar() {
                       </a>
                   </div>
                   <div class="absolute bottom-0 my-10">
-                      <a class="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-100 transition-colors duration-200 flex items-center py-2 px-8" href="#">
+                      <a class="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-100 transition-colors duration-200 flex items-center py-2 px-8" href="/auth/logout">
                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24"><path d="M19,3 C20.11,3 21,3.9 21,5 L21,8 L19,8 L19,5 L5,5 L5,19 L19,19 L19,16 L21,16 L21,19 C21,20.1 20.11,21 19,21 L5,21 C3.9,21 3,20.1 3,19 L3,5 C3,3.9 3.9,3 5,3 L19,3 Z M15.5,17 L20.5,12 L15.5,7 L14.09,8.41 L16.67,11 L7,11 L7,13 L16.67,13 L14.09,15.59 L15.5,17 Z"/></svg>
                           <span class="mx-4 font-medium">
                               Log Out
