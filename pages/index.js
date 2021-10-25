ad64e4332f6b0ede9aa668ec6eab174b43b7900f
ntaheij <noah@taheij.nl>
2021-10-25 12:54:06 +0200

    Added sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Sidebar from "../components/Sidebar";
+
 export default function Home() {
   return (
-    <div>
-      Initializing...
+    <div className="grid grid-cols-2 md:grid-cols-6">
+      <Sidebar />
     </div>
   )
 }
