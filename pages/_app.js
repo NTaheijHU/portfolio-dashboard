b047ea4cabec52e405ae188cd918022864d64d41
ntaheij <noah@taheij.nl>
2021-10-25 10:49:57 +0200

    Initial Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import "tailwindcss/base.css";
+import "tailwindcss/components.css";
+import "tailwindcss/utilities.css";
+import "tailwindcss/screens.css";
+import "../styles/app.css";
+import "../styles/arrow.css";
 
-function MyApp({ Component, pageProps }) {
+function Dashboard({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default Dashboard;
