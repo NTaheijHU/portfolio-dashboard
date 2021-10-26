ae8b817d4ec5e5f7439905c4c8c4fcaf9afff646
ntaheij <noah@taheij.nl>
2021-10-26 12:55:42 +0200

    Added about content fill

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import FormTextArea from "../components/forms/FormTextArea";
 import Sidebar from "../components/Sidebar";
 import TitleCard from "../components/cards/TitleCard";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
       <Sidebar 
@@ -14,19 +14,25 @@ export default function Home() {
         <TitleCard text="Over Mij" />
         <div className="ml-12 mr-12 mt-4">
           <form className="grid grid-cols-2">
-            <FormItem title="Naam" />
-            <FormItem title="Title" />
-            <FormItem title="Image URL" />
-            <FormItem title="Github URL" />
-            <FormTextArea title="About Text" />
-            <FormTextArea title="About Text Long" />
-            <FormItem title="Projects Title" />
-            <FormItem title="Projects Image URL" />
-            <FormTextArea title="Projects Text" />
+            <FormItem title="Naam" value={props.about.name} />
+            <FormItem title="Title" value={props.about.title} />
+            <FormItem title="Image URL" value={props.about.image} />
+            <FormItem title="Github URL" value={props.about.githubLink} />
+            <FormTextArea title="About Text" value={props.about.aboutText} />
+            <FormTextArea title="About Text Long" value={props.about.aboutTextLong} />
+            <FormItem title="Projects Title" value={props.about.projectsTitle} />
+            <FormItem title="Projects Image URL" value={props.about.projectsImage} />
+            <FormTextArea title="Projects Text" value={props.about.projectsText }/>
             <FormButton right/>
           </form>
         </div>
       </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const data = await fetch(process.env.API_ENDPOINT + '/about');
+  const about = await data.json();
+  return { props: { about: about[0] } }
 }
