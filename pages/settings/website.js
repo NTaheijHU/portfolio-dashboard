fc3d1e3c7a70281b37a6d1aa7e7841b6325692f5
ntaheij <noah@taheij.nl>
2021-11-03 11:56:06 +0100

    Fixed linking

--- pages/settings/website.js
+++ pages/settings/website.js
@@ -13,7 +13,7 @@ export default function Home() {
           <div className="bg-gray-200 dark:bg-gray-900 border-2 border-gray-300 dark:border-gray-700 text-gray-800 dark:text-gray-200 rounded-xl mt-8 mx-8 md:mx-20 flex items-center justify-center h-1/6">
             <p className="text-3xl font-semibold">Website Settings</p>
           </div>
-          <a className="hover:cursor-pointer" onClick={() => { window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" }}>
+          <a className="hover:cursor-pointer" onClick={() => { window.location.href = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" }}>
             <div className="bg-red-600 hover:bg-red-700 rounded-xl mt-8 mx-8 md:mx-20 p-0 flex items-center justify-center h-1/2">
               <h1 className="text-center text-6xl">Verwijder Website</h1>
             </div>
